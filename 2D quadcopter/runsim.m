aeef73cc7cad7b5978ae187a17b4a15b9a700238
yaswanth1701 <92177410+yaswanth1701@users.noreply.github.com>
2022-03-23 01:14:28 +0530

    Add files via upload

---
+++ 2D quadcopter/runsim.m
@@ -0,0 +1,15 @@
+close all;
+
+addpath('utils');
+addpath('trajectories');
+
+controlhandle = @controller;
+
+% Choose which trajectory you want to test with
+ %trajhandle = @traj_line;
+ trajhandle = @traj_sine;
+
+[t, state] = simulation_2d(controlhandle, trajhandle);
+
+
+
